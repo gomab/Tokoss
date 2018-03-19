b2ef349945b1367b590c07b8b2ad628e8f2a9f64
gomab <egoiomab@gamil.com>
2018-03-19 17:15:22 +0100

    Display user info and logout

--- core/classes/User.php
+++ core/classes/User.php
@@ -31,4 +31,17 @@
 				return false;
 			}
 		}
+
+		public function userData($user_id){
+			$stmt = $this->pdo->prepare("SELECT * FROM `users` WHERE `user_id` = :user_id ");
+			$stmt->bindParam(":user_id", $user_id, PDO::PARAM_INT);
+			$stmt->execute();
+			return $stmt->fetch(PDO::FETCH_OBJ);
+		}
+
+		public function logout(){
+			$_SESSION = [];
+			session_destroy();
+			header('Location: ../index.php');
+		}
 	}
